6b843b2bfe1ae0691d1ae0de3f72b7200c1e5876
lfarrell <lfarrell@iconarch.com>
2022-07-24 17:37:35 -0400

    Added Session 4 Bonus - Dialogs with Elements by LineType

---
+++ RevitAddinAcademy/FrmWallsFromLines.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace RevitAddinAcademy
+{
+    partial class FrmWallsFromLines
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbLineStyles = new System.Windows.Forms.ComboBox();
+            this.cmbWallTypes = new System.Windows.Forms.ComboBox();
+            this.tbxWallHeight = new System.Windows.Forms.TextBox();
+            this.cbxStructural = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Line Style to Convert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Wall Type to Convert";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Wall Height";
+            // 
+            // cmbLineStyles
+            // 
+            this.cmbLineStyles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbLineStyles.FormattingEnabled = true;
+            this.cmbLineStyles.Location = new System.Drawing.Point(27, 47);
+            this.cmbLineStyles.Name = "cmbLineStyles";
+            this.cmbLineStyles.Size = new System.Drawing.Size(509, 24);
+            this.cmbLineStyles.TabIndex = 3;
+            // 
+            // cmbWallTypes
+            // 
+            this.cmbWallTypes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbWallTypes.FormattingEnabled = true;
+            this.cmbWallTypes.Location = new System.Drawing.Point(27, 123);
+            this.cmbWallTypes.Name = "cmbWallTypes";
+            this.cmbWallTypes.Size = new System.Drawing.Size(509, 24);
+            this.cmbWallTypes.TabIndex = 4;
+            // 
+            // tbxWallHeight
+            // 
+            this.tbxWallHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxWallHeight.Location = new System.Drawing.Point(27, 202);
+            this.tbxWallHeight.Name = "tbxWallHeight";
+            this.tbxWallHeight.Size = new System.Drawing.Size(509, 22);
+            this.tbxWallHeight.TabIndex = 6;
+            // 
+            // cbxStructural
+            // 
+            this.cbxStructural.AutoSize = true;
+            this.cbxStructural.Location = new System.Drawing.Point(15, 254);
+            this.cbxStructural.Name = "cbxStructural";
+            this.cbxStructural.Size = new System.Drawing.Size(151, 20);
+            this.cbxStructural.TabIndex = 7;
+            this.cbxStructural.Text = "Make Wall Structural";
+            this.cbxStructural.UseVisualStyleBackColor = true;
+            this.cbxStructural.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(272, 290);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(124, 35);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(412, 290);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(124, 35);
+            this.btnOK.TabIndex = 9;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // FrmWallsFromLines
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 345);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.cbxStructural);
+            this.Controls.Add(this.tbxWallHeight);
+            this.Controls.Add(this.cmbWallTypes);
+            this.Controls.Add(this.cmbLineStyles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmWallsFromLines";
+            this.Text = "Create walls from lines";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbLineStyles;
+        private System.Windows.Forms.ComboBox cmbWallTypes;
+        private System.Windows.Forms.TextBox tbxWallHeight;
+        private System.Windows.Forms.CheckBox cbxStructural;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
